184e495b6fe9853886532c9c7d16053d38d0b7b4
Piyush Kumar <piyush.kumar@Triconinfotech.com>
2021-04-17 00:47:41 +0530

    Revert "fix: another method added"
    This reverts commit 03b9a32373289b3d0e38750e2a1680559e54c77c.

--- src/main/java/com/async/config/Configuration.java
+++ src/main/java/com/async/config/Configuration.java
@@ -5,11 +5,5 @@ public class Configuration {
 	public void testMethod() {
 		System.out.println("test method");
 	}
-	
-	
-	public void test1Method() {
-		System.out.println("test1 method");
-	}
-	
-	
+
 }
