8eea90e41479db67569b457864371c20e88eea6c
Piyush Kumar <piyush.kumar@Triconinfotech.com>
2021-04-18 16:58:21 +0530

    Revert "fix: application runner added"
    This reverts commit 4dc4a9b0f23164fe97bb07cbe357b7c4cac6be5e.

--- src/main/java/com/async/AsyncApplication.java
+++ src/main/java/com/async/AsyncApplication.java
@@ -1,14 +1,12 @@
 package com.async;
 
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class AsyncApplication implements ApplicationRunner {
+public class AsyncApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AsyncApplication.class, args);
@@ -17,13 +15,5 @@ public class AsyncApplication implements ApplicationRunner {
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
-	}
-
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		// TODO Auto-generated method stub
-		
-		System.out.println("application runner added");
-		
 	}
 }
