4dc4a9b0f23164fe97bb07cbe357b7c4cac6be5e
Piyush Kumar <piyush.kumar@Triconinfotech.com>
2021-04-18 16:17:25 +0530

    fix: application runner added

--- src/main/java/com/async/AsyncApplication.java
+++ src/main/java/com/async/AsyncApplication.java
@@ -1,12 +1,14 @@
 package com.async;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class AsyncApplication {
+public class AsyncApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AsyncApplication.class, args);
@@ -15,5 +17,13 @@ public class AsyncApplication {
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		System.out.println("application runner added");
+		
 	}
 }
